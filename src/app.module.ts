4ca6ed27a6ef9619ae41509c60de6f7110caeab3
CyrilOkidi <okidicyril@gmail.com>
2023-09-26 20:10:12 +0300

    Generated drivers module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbConfig } from './config/db.config';
+import { DriversModule } from './drivers/drivers.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { dbConfig } from './config/db.config';
       isGlobal: true,
     }),
     TypeOrmModule.forRootAsync(dbConfig),
+    DriversModule,
   ],
   controllers: [AppController],
   providers: [AppService],
