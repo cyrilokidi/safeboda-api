b3602d2ea8e26c063dad86d98709096d172f8c2a
CyrilOkidi <okidicyril@gmail.com>
2023-09-28 07:36:38 +0300

    Add auth. guards to endpoints to be protected

--- src/drivers/drivers.controller.ts
+++ src/drivers/drivers.controller.ts
@@ -6,26 +6,31 @@ import {
   ParseUUIDPipe,
   Delete,
   HttpCode,
+  UseGuards,
 } from '@nestjs/common';
 import { DriversService } from './drivers.service';
 import { CreateDriverDto } from './dto/create-driver.dto';
 import { Driver } from './entities/driver.entity';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('drivers')
 export class DriversController {
   constructor(private readonly driversService: DriversService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createDriverDto: CreateDriverDto): Promise<Driver> {
     return this.driversService.create(createDriverDto);
   }
 
+  @UseGuards(AuthGuard)
   @Post(':id/suspended')
   @HttpCode(204)
   suspend(@Param('id', ParseUUIDPipe) driverId: string): Promise<void> {
     return this.driversService.suspend(driverId);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id/suspended')
   @HttpCode(204)
   deleteSuspend(@Param('id', ParseUUIDPipe) driverId: string): Promise<void> {
