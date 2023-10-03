f5ffe14c66c2976ac505d4fd8af13c4d3fd35b64
CyrilOkidi <okidicyril@gmail.com>
2023-10-03 08:19:39 +0300

    Resolve service unit tests

--- src/drivers/drivers.controller.spec.ts
+++ src/drivers/drivers.controller.spec.ts
@@ -2,11 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { DriversController } from './drivers.controller';
 import { DriversService } from './drivers.service';
 import { AuthGuard } from '../auth/auth.guard';
+import { CreateDriverDto } from './dto/create-driver.dto';
 
 describe('DriversController', () => {
   let controller: DriversController;
 
-  const mockDriversService = {};
+  const mockDriversService = {
+    create: jest.fn((createDriverDto: CreateDriverDto) => createDriverDto),
+  };
 
   const mockAuthGuard = {};
 
@@ -26,5 +29,13 @@ describe('DriversController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a new driver', () => {
+    const newDriver: CreateDriverDto = {
+      name: 'John Doe',
+      phone: '+254700000001',
+    };
+    expect(controller.create(newDriver)).toEqual(newDriver);
   });
 });
