ebadb74786abbd79c356da0b348aa21597866e30
CyrilOkidi <okidicyril@gmail.com>
2023-10-03 21:27:52 +0300

    Create controller test suit to add new passenger

--- src/passengers/passengers.controller.spec.ts
+++ src/passengers/passengers.controller.spec.ts
@@ -2,13 +2,20 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PassengersController } from './passengers.controller';
 import { PassengersService } from './passengers.service';
 import { AuthGuard } from '../auth/auth.guard';
+import { CreatePassengerDto } from './dto/create-passenger.dto';
 
 describe('PassengersController', () => {
   let passengersController: PassengersController;
 
   const mockAuthGuard = {};
 
-  const mockPassengersService = {};
+  const mockPassengersService = {
+    create: jest.fn((createPassengerDto: CreatePassengerDto) =>
+      Promise.resolve({
+        ...createPassengerDto,
+      }),
+    ),
+  };
 
   beforeEach(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
@@ -27,5 +34,15 @@ describe('PassengersController', () => {
 
   it('should be defined', () => {
     expect(passengersController).toBeDefined();
+  });
+
+  it('should create a new passenger', async () => {
+    const newPassenger: CreatePassengerDto = {
+      name: 'John Doe',
+      phone: '+254700000001',
+    };
+    expect(await passengersController.create(newPassenger)).toEqual({
+      ...newPassenger,
+    });
   });
 });
