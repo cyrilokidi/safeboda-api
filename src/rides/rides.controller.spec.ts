e77f0579462af48795a1495a524f9ddb7aa971fb
CyrilOkidi <okidicyril@gmail.com>
2023-10-03 19:54:09 +0300

    Create test suite to add new ride

--- src/rides/rides.controller.spec.ts
+++ src/rides/rides.controller.spec.ts
@@ -3,6 +3,7 @@ import { RidesController } from './rides.controller';
 import { RidesService } from './rides.service';
 import { AuthGuard } from '../auth/auth.guard';
 import { RidesPageOptionsDto } from './dto/rides-page-options.dto';
+import { CreateRideDto } from './dto/create-ride.dto';
 
 describe('RidesController', () => {
   let ridesController: RidesController;
@@ -10,6 +11,14 @@ describe('RidesController', () => {
   const mockAuthGuard = {};
 
   const mockRidesService = {
+    create: jest.fn(
+      (passengerId: string, driverId: string, createRideDto: CreateRideDto) =>
+        Promise.resolve({
+          passengerId,
+          driverId,
+          ...createRideDto,
+        }),
+    ),
     findAll: jest.fn((ridesPageOptionsDto: RidesPageOptionsDto) =>
       Promise.resolve({ ...ridesPageOptionsDto }),
     ),
@@ -31,6 +40,24 @@ describe('RidesController', () => {
 
   it('should be defined', () => {
     expect(ridesController).toBeDefined();
+  });
+
+  it('should create a new ride', async () => {
+    const passengerId = '1';
+    const driverId = '1';
+    const createRideDto: CreateRideDto = {
+      pickupPointLatitude: 1.2921,
+      pickupPointLongitude: 36.8219,
+      destinationLatitude: 1.2921,
+      destinationLongitude: 36.8219,
+    };
+    expect(
+      await ridesController.create(passengerId, driverId, createRideDto),
+    ).toEqual({
+      passengerId,
+      driverId,
+      ...createRideDto,
+    });
   });
 
   it('should find all ongoing rides', async () => {
