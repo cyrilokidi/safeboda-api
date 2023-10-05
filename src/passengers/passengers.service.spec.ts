e28ed4ff347180f7a243045909f53f14afce83cd
CyrilOkidi <okidicyril@gmail.com>
2023-10-05 18:54:28 +0300

    Add passenger service unit test case

--- src/passengers/passengers.service.spec.ts
+++ src/passengers/passengers.service.spec.ts
@@ -1,24 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PassengersService } from './passengers.service';
-import { DataSource } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { testDbConfig } from '../../src/config/db.config';
+import { faker } from '@faker-js/faker';
+import { CreatePassengerDto } from './dto/create-passenger.dto';
 
 describe('PassengersService', () => {
-  let service: PassengersService;
-
-  const mockDataSource = {};
+  let passengersService: PassengersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PassengersService, DataSource],
-    })
-      .overrideProvider(DataSource)
-      .useValue(mockDataSource)
-      .compile();
+      imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRootAsync(testDbConfig),
+      ],
+      providers: [PassengersService],
+    }).compile();
 
-    service = module.get<PassengersService>(PassengersService);
+    passengersService = module.get<PassengersService>(PassengersService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(passengersService).toBeDefined();
+  });
+
+  it('should return new passenger details', async () => {
+    const createPassengerDto: CreatePassengerDto = {
+      name: faker.person.fullName(),
+      phone: '+25510000001',
+    };
+    const response = await passengersService.create(createPassengerDto);
+    expect(response).toEqual({
+      id: expect.any(String),
+      createdAt: expect.any(Date),
+      ...createPassengerDto,
+    });
   });
 });
